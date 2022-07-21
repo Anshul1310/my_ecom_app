9695a3ee4433384c5afacc2146a3a7180d328784
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2022-07-21 22:24:15 +0530

    Update index.js

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 
 const api = axios.create({
-	baseURL: "http://localhost:4000",
+	baseURL: "http://wangari.et",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
